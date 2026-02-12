cb422f9af35ff7378bc2cc8b7764fe6f56495cc7
Anadasu Shethu Babu <shethu.babu@bounteous.com>
2026-02-12 23:48:26 +0530

    Button

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,5 @@
 import { useCartStore } from '@/store/cartStore';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/Button';
 import { Trash2, Minus, Plus } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
